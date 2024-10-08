7c1a7cca71736df6b07b54a03fdb709777140ef5
ongkenith <kenith.ong.2023@scis.smu.edu.sg>
2024-10-08 17:13:24 +0800

    Added form

--- my-app/app/quiz/page.tsx
+++ my-app/app/quiz/page.tsx
@@ -1,5 +1,128 @@
+"use client"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }).max(10, {
+    message: "Username must be below 11 characters."
+  }),
+  question1: z.string().min(1,{
+    message: "Please enter your answer"
+  }),
+  question2: z.string({
+    message: "Please enter your answer"
+  }),
+})
+
 export default function quiz() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      question1: "",
+    },
+  })
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
     return (
-      <div>Hello World, this is the quiz route</div>
+      <div className="mx-4">
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input type="string" placeholder="Username" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="question1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Question 1</FormLabel>
+              <FormDescription className="text-grey-100">
+                Why are drugs bad?
+              </FormDescription>
+              <FormControl>
+                <Input type="string" placeholder="Answer" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="question2"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Question 2</FormLabel>
+              <FormDescription className="text-grey-100">
+                Why should you do if your friends or family are consuming drugs?
+              </FormDescription>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="seekhelp" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Seek Help
+                    </FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="nothing" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Do nothing
+                    </FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    </div>
     );
   }
